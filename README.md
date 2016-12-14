37e81f359c490fe2ba6d3377953cbc8217685e27
Stephane Karraz <stephane.karraz@epitech.eu>
2016-12-14 15:03:45 +0100

    init symfony project

---
+++ README.md
@@ -0,0 +1,4 @@
+TrenteDouze
+===========
+
+A Symfony project created on December 14, 2016, 1:43 pm.
---
+++ src/SimpleBundle/SimpleBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SimpleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SimpleBundle extends Bundle
+{
+}
