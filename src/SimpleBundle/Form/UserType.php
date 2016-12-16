81791aeb18fc29d56fab344082cdddf539eab148
Stephane Karraz <stephane.karraz@epitech.eu>
2016-12-16 11:34:11 +0100

    Customizing CRUD Form

--- src/SimpleBundle/Form/UserType.php
+++ src/SimpleBundle/Form/UserType.php
@@ -5,6 +5,10 @@ namespace SimpleBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 
 class UserType extends AbstractType
 {
@@ -13,9 +17,9 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('firstname')->add('lastname')->add('email')->add('password')        ;
+        $builder->add('firstname')->add('lastname')->add('email', EmailType::class)->add('password', PasswordType::class)        ;
     }
-    
+
     /**
      * {@inheritdoc}
      */
